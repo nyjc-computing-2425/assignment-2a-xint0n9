3a356f699d5f44fcf773c454d5ff9ff587a509ac
xint0n9.l <160615573+xint0n9@users.noreply.github.com>
2024-03-01 02:55:55 +0000

    Final submissionn

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ num = num.strip()
 numm = num.replace(".","")
 numm = numm.lstrip("0")
 sf = len(numm)
-# do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
